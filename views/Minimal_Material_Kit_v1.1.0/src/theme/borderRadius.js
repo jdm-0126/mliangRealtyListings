a938ae9ef08ee6a110ef5335ecac4df580d13c4c
joseph maninang <jn16h7@gmail.com>
2026-07-09 00:13:56 +0800

    clean up

--- views/Minimal_Material_Kit_v1.1.0/src/theme/borderRadius.js
+++
@@ -1,9 +0,0 @@
-// ----------------------------------------------------------------------
-
-const BorderRadius = {
-  borderRadius: 8,
-  borderRadiusSm: 12,
-  borderRadiusMd: 16
-};
-
-export default BorderRadius;
